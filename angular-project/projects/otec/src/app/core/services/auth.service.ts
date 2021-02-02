03efcc9503e8d59e4b5432d8c4a1b052f06548b8
jorge <jorge.retama@zweicom.com>
2021-02-02 08:59:16 -0300

    Se ccambia la url del api

--- angular-project/projects/otec/src/app/core/services/auth.service.ts
+++ angular-project/projects/otec/src/app/core/services/auth.service.ts
@@ -216,7 +216,7 @@ export class AuthService {
       // Password: password,
     };
     return (this.httpClient as HttpClient).post<LoginModel.AuthLoginResponse>(
-      `${this.apiBase}/OTEC/login`,
+      `${this.apiBase}d/login`,
       JSON.stringify(data)
     );
   }
