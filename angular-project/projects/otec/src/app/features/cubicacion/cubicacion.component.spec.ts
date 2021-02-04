79786a262ab7c24b61ebe68bac848c907b43996f
jorge <jorge.retama@zweicom.com>
2021-02-04 17:39:52 -0300

    Se crea modulo de cubicacion

---
+++ angular-project/projects/otec/src/app/features/cubicacion/cubicacion.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CubicacionComponent } from './cubicacion.component';
+
+describe('CubicacionComponent', () => {
+  let component: CubicacionComponent;
+  let fixture: ComponentFixture<CubicacionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CubicacionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CubicacionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-project/projects/otec/src/app/features/cubicacion/cubicacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'otec-cubicacion',
+  templateUrl: './cubicacion.component.html',
+  styleUrls: ['./cubicacion.component.css']
+})
+export class CubicacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
