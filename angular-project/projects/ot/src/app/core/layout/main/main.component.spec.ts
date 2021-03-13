0d78ebb0251ed45c2b1de94aeac02bff6bcd267f
jorge <jorge.retama@zweicom.com>
2021-03-13 01:21:59 -0300

    navbar apararece solo si está logeado

--- angular-project/projects/ot/src/app/core/layout/main/main.component.spec.ts
+++ angular-project/projects/ot/src/app/core/layout/main/main.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AuthService } from '@coreOT/services/auth.service';
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
+  let authService: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
-    })
-    .compileComponents();
+      declarations: [MainComponent],
+      providers: [{ provide: 'environment', useValue: {} }],
+    }).compileComponents();
+    authService = TestBed.inject(AuthService);
   });
 
   beforeEach(() => {
@@ -21,5 +23,10 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should login true', () => {
+    spyOn(authService, 'isLoggedIn').and.returnValue(true);
+    expect(component.isLoggedIn()).toBe(true);
   });
 });
--- angular-project/projects/ot/src/app/core/layout/main/main.component.ts
+++ angular-project/projects/ot/src/app/core/layout/main/main.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '@coreOT/services/auth.service';
 @Component({
   selector: 'ot-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
-
 }
