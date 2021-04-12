96e157f7ed1bb793f5cb21adcb20dedf8f2e6f12
jorge <jorge.retama@zweicom.com>
2021-04-12 12:19:14 -0300

    popup y snackbar

--- angular-project/src/app/ui/popup/popup.component.ts
+++ angular-project/src/app/ui/popup/popup.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.scss']
+  styleUrls: ['./popup.component.scss'],
+  providers: [ConfirmationService],
 })
 export class PopupComponent implements OnInit {
-
-  constructor() { }
+  @Input() public event: Event;
+  @Output() public CloseEvent: EventEmitter<any> = new EventEmitter();
+  constructor(private confirmationService: ConfirmationService) {}
 
   ngOnInit(): void {
+    this.confirmationService.confirm({
+      target: this.event.target as EventTarget,
+      message: `Si cambia de valor se borrara todos los sevicios seleccionados. Está seguro que desea proceder?`,
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {},
+      reject: () => {},
+    });
   }
-
 }
