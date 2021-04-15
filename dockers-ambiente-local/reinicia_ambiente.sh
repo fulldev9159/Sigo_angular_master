a1696d41244b687d592236ba1c6e2308408bf142
jorge <jorge.retama@zweicom.com>
2021-04-15 13:01:26 -0300

    nuevo contenedor robotframeword

--- dockers-ambiente-local/reinicia_ambiente.sh
+++ dockers-ambiente-local/reinicia_ambiente.sh
@@ -4,3 +4,5 @@ docker pull registryv2.zweicom.com:5000/mcl-otec-webclient:latest-develop
 docker-compose up -d mcl_sigo_test_web
 
 cd demos-env/; docker-compose stop mcl_api_otec mcl_otec_db mcl_sigo_test_web;docker-compose rm -f  mcl_api_otec mcl_otec_db mcl_sigo_test_web;docker pull registryv2.zweicom.com:5000/mcl-otec-webclient:latest-develop;docker-compose up -d mcl_sigo_test_web
+
+for i in $(docker ps -a|grep robot|awk '{print $1}'); do echo $i; docker rm $i;done
