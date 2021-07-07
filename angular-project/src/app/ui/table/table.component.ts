6da6cb89cdd6a6644c97a6e7775056df60b8acbd
César Agustín <cesar.gonzalez@zweicom.com>
2021-07-07 16:09:53 -0400

    #3 dynamic actions on table

--- angular-project/src/app/ui/table/table.component.ts
+++ angular-project/src/app/ui/table/table.component.ts
@@ -7,6 +7,8 @@ import {
 } from '@angular/core';
 import { Subject } from 'rxjs';
 
+import { Config } from './config';
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -15,7 +17,7 @@ import { Subject } from 'rxjs';
 })
 export class TableComponent implements OnInit, OnDestroy {
   // declarations
-  @Input() public config: any;
+  @Input() public config: Config;
   @Input() public items: any[];
   private destroyInstance: Subject<boolean> = new Subject();
 
@@ -26,5 +28,15 @@ export class TableComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroyInstance.next(true);
     this.destroyInstance.complete();
+  }
+
+  typeAction(actions: any): string {
+    return typeof actions;
+  }
+
+  context(actions: any): any {
+    return {
+      actions,
+    };
   }
 }
