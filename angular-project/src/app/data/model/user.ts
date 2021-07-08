3af90113091bf9fcd0cf41ab3e33ee8d5b8b4f65
César Agustín <cesar.gonzalez@zweicom.com>
2021-07-08 15:53:54 -0400

    #4 add coordinators api calls

---
+++ angular-project/src/app/data/model/user.ts
@@ -0,0 +1,38 @@
+export interface User {
+  id: number;
+  username: string;
+  rut: string;
+  nombres: string;
+  apellidos: string;
+  celular: string;
+  activo: boolean;
+  firma: string;
+  proveedor_id: number;
+  area_id: number;
+  email: string;
+  created_at: string;
+  updated_at: string;
+  proveedor_nombre: string;
+  area_nombre: string;
+}
+
+export interface UsersResponse {
+  data: {
+    items: User[];
+  };
+
+  pagination: {
+    total_pages: number;
+    items_per_page: number;
+    field_order: {
+      [index: string]: string;
+    }[];
+    page: number;
+    total_items: number;
+  };
+
+  status: {
+    description: string;
+    responseCode: number;
+  };
+}
--- angular-project/src/app/data/service/ot.service.ts
+++ angular-project/src/app/data/service/ot.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { OT, OTsResponse } from '../model';
+import { User, UsersResponse } from '../model';
 
 @Injectable({
   providedIn: 'root',
@@ -28,12 +29,91 @@ export class OTService {
   }
 
   approveOT(perfil_id: number, otID: number): Observable<any> {
-    console.log('approve OT still not implemented');
+    console.log('[TODO] approve OT still not implemented', {
+      perfil_id,
+      otID,
+    });
     return of({});
   }
 
   rejectOT(perfil_id: number, otID: number): Observable<any> {
-    console.log('reject OT still not implemented');
+    console.log('[TODO] reject OT still not implemented', {
+      perfil_id,
+      otID,
+    });
+    return of({});
+  }
+
+  getCoordinators(perfil_id: number, otID: number): Observable<User[]> {
+    console.log('[TODO] get OT coordinators still not implemented', {
+      perfil_id,
+      otID,
+    });
+    return of([
+      {
+        id: 3,
+        username: 'erickuc',
+        rut: '13237732',
+        nombres: 'Erick',
+        apellidos: 'Urrutia Correa',
+        celular: '56987692645',
+        activo: true,
+        firma: '',
+        proveedor_id: 2,
+        area_id: 2,
+        email: 'erick.urrutia@ericsson.com',
+        created_at: '2020-01-01 00:00:00',
+        updated_at: '2020-01-01 00:00:00',
+        proveedor_nombre: 'COASIN',
+        area_nombre: 'Contratista',
+      },
+      {
+        id: 4,
+        username: 'jaimecc',
+        rut: '6776815',
+        nombres: 'Jaime ',
+        apellidos: 'Contreras Cortes',
+        celular: '56973971516',
+        activo: true,
+        firma: '',
+        proveedor_id: 2,
+        area_id: 2,
+        email: 'jaime.contreras@ericsson.com',
+        created_at: '2020-01-01 00:00:00',
+        updated_at: '2020-01-01 00:00:00',
+        proveedor_nombre: 'COASIN',
+        area_nombre: 'Contratista',
+      },
+      {
+        id: 7,
+        username: 'gen175708502',
+        rut: '17570850',
+        nombres: 'KAREN ELIS',
+        apellidos: 'RIQUELME TORRES',
+        celular: '56969188330',
+        activo: true,
+        firma: '',
+        proveedor_id: 5,
+        area_id: 2,
+        email: 'karen.riquelme@generatel.cl',
+        created_at: '2020-01-01 00:00:00',
+        updated_at: '2020-01-01 00:00:00',
+        proveedor_nombre: 'AJ INGENIEROS S.A.',
+        area_nombre: 'Contratista',
+      },
+    ]);
+  }
+
+  assignCoordinator(
+    perfil_id: number,
+    otID: number,
+    coordinatorID: number
+  ): Observable<any> {
+    console.log('[TODO] assign coordinator to OT still not implemented', {
+      perfil_id,
+      otID,
+      coordinatorID,
+    });
     return of({});
   }
 }
