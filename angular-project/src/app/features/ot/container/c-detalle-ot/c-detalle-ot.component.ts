e368b63da71d2edabae70ca08b983af0116e40da
jorge <jorge.retama@zweicom.com>
2021-07-12 13:54:52 -0400

    Se depliegan las LPUs de una OT

--- angular-project/src/app/features/ot/container/c-detalle-ot/c-detalle-ot.component.ts
+++ angular-project/src/app/features/ot/container/c-detalle-ot/c-detalle-ot.component.ts
@@ -7,7 +7,9 @@ import {
 import { Subscription, Subject, Observable } from 'rxjs';
 import { OtFacade } from '@storeOT/features/ot/ot.facade';
 import * as otModel from '@storeOT/features/ot/ot.model';
+import * as cubModel from '@storeOT/features/cubicacion/cubicacion.model';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CubicacionFacade } from '@storeOT/features/cubicacion/cubicacion.facade';
 
 @Component({
   selector: 'app-c-detale-ot',
@@ -18,7 +20,13 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 export class CDetalleOtComponent implements OnInit, OnDestroy {
   subscription: Subscription = new Subscription();
   public detalleOt$: Observable<otModel.DataRspDetalleOT>;
-  constructor(private otFacade: OtFacade, private rutaActiva: ActivatedRoute) {}
+  public DisplayModal = false;
+  public detalleCubicacion$: Observable<cubModel.ResponseDetalleCubicacion[]>;
+  constructor(
+    private otFacade: OtFacade,
+    private rutaActiva: ActivatedRoute,
+    private cubageFacade: CubicacionFacade
+  ) {}
 
   ngOnInit(): void {
     this.subscription.add(
@@ -28,11 +36,23 @@ export class CDetalleOtComponent implements OnInit, OnDestroy {
         }
       })
     );
-
     this.detalleOt$ = this.otFacade.getDetalleOtSelector$();
+    this.detalleCubicacion$ =
+      this.cubageFacade.getDetallesCubicacionSelector$();
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  getlpus(cubicacion_id: number): void {
+    console.log(cubicacion_id);
+    this.DisplayModal = true;
+    this.cubageFacade.getDetallesCubicacionAction(cubicacion_id);
+  }
+
+  Close(): void {
+    console.log('Close desde Container ot detalle-ot');
+    this.DisplayModal = false;
   }
 }
