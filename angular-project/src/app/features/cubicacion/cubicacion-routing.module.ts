81c193faf0212e75a1ad17236fefacd8816a7371
jorge <jorge.retama@zweicom.com>
2021-07-29 16:16:32 -0400

    se configura guard para acceso al modulo de Cubicaciones

--- angular-project/src/app/features/cubicacion/cubicacion-routing.module.ts
+++ angular-project/src/app/features/cubicacion/cubicacion-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormCubContainerComponent } from './container/form-cub/form-cub-container.component';
 import { ListCubComponent } from './container/list-cub/list-cub.component';
 import { CubicacionComponent } from './cubicacion.component';
+import { NgxPermissionsGuard } from 'ngx-permissions';
 
 export const routes: Routes = [
   {
@@ -13,17 +14,38 @@ export const routes: Routes = [
       {
         path: 'list-cub',
         component: ListCubComponent,
-        data: { state: 'list-cub' },
+        canActivate: [NgxPermissionsGuard],
+        data: {
+          permissions: {
+            only: 'CUBICACION_LISTAR',
+            redirectTo: '/app/ot/list-ot',
+          },
+          state: 'list-cub',
+        },
       },
       {
         path: 'form-cub',
         component: FormCubContainerComponent,
-        data: { state: 'form-cub' },
+        canActivate: [NgxPermissionsGuard],
+        data: {
+          permissions: {
+            only: 'CUBICACION_CREAR',
+            redirectTo: '/app/ot/list-ot',
+          },
+          state: 'form-cub',
+        },
       },
       {
         path: 'form-cub/:id',
         component: FormCubContainerComponent,
-        data: { state: 'form-cub' },
+        canActivate: [NgxPermissionsGuard],
+        data: {
+          permissions: {
+            only: 'CUBICACION_EDITAR',
+            redirectTo: '/app/ot/list-ot',
+          },
+          state: 'form-cub',
+        },
       },
     ],
   },
