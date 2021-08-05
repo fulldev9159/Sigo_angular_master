a330598d97095f2637e07c003271ef5d3f5dc02f
César Agustín <cesar.gonzalez@zweicom.com>
2021-08-05 13:05:14 -0400

    create new cubage form validating the name

---
+++ angular-project/src/app/features/cubicacion/container/form-cub2/form-cub-container.component.ts
@@ -0,0 +1,78 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable, Subscription, of } from 'rxjs';
+
+import { CubicacionFacade } from '@storeOT/features/cubicacion/cubicacion.facade';
+import * as CubModel from '@storeOT/features/cubicacion/cubicacion.model';
+
+@Component({
+  selector: 'app-form-cub2-container',
+  templateUrl: './form-cub-container.component.html',
+  styleUrls: ['./form-cub-container.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FormCub2ContainerComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription();
+
+  autoSuggestItems$: Observable<CubModel.AutoSuggestItem[]> = of([]);
+
+  formControls = {
+    nombre: new FormControl('', [
+      Validators.required,
+      this.noWhitespace,
+      Validators.maxLength(300),
+    ]),
+    // cubicacion_id: null,
+    // nombre: [null, [Validators.required, Validators.maxLength(300)]],
+    // contrato_marco_id: [null, Validators.required],
+    // subcontrato_id: null,
+    // proveedor_id: [null, Validators.required],
+    // region_id: [null, Validators.required],
+    // tipo_servicio_id: null,
+    // lpus: [],
+  };
+
+  formCubicacion: FormGroup = new FormGroup(this.formControls);
+
+  noWhitespace(control: FormControl): any {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
+  }
+
+  constructor(private cubageFacade: CubicacionFacade) {}
+
+  ngOnInit(): void {
+    this.cubageFacade.resetData();
+    this.autoSuggestItems$ = this.cubageFacade.getAutoSuggestSelector$();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  goBack(): void {
+    this.cubageFacade.resetData();
+  }
+
+  // acciones del autocompletado con el campo 'nombre'
+  nameSelected(event: any): void {
+    console.log('name selected', event);
+  }
+
+  nameChanged(name: string): void {
+    this.cubageFacade.getAutoSuggestAction(name, 5);
+  }
+
+  nameFocused(event: any): void {}
+
+  nameCleared(event: any): void {
+    this.cubageFacade.getAutoSuggestAction('', 5);
+  }
+}
--- angular-project/src/app/features/cubicacion/cubicacion-routing.module.ts
+++ angular-project/src/app/features/cubicacion/cubicacion-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormCubContainerComponent } from './container/form-cub/form-cub-container.component';
+import { FormCub2ContainerComponent } from './container/form-cub2/form-cub-container.component';
 import { ListCubComponent } from './container/list-cub/list-cub.component';
 import { CubicacionComponent } from './cubicacion.component';
 import { NgxPermissionsGuard } from 'ngx-permissions';
@@ -45,6 +46,30 @@ export const routes: Routes = [
             redirectTo: '/app/ot/list-ot',
           },
           state: 'form-cub',
+        },
+      },
+      {
+        path: 'form-cub2',
+        component: FormCub2ContainerComponent,
+        canActivate: [NgxPermissionsGuard],
+        data: {
+          permissions: {
+            only: 'CUBICACION_CREAR',
+            redirectTo: '/app/ot/list-ot',
+          },
+          state: 'form-cub2',
+        },
+      },
+      {
+        path: 'form-cub2/:id',
+        component: FormCub2ContainerComponent,
+        canActivate: [NgxPermissionsGuard],
+        data: {
+          permissions: {
+            only: 'CUBICACION_EDITAR',
+            redirectTo: '/app/ot/list-ot',
+          },
+          state: 'form-cub2',
         },
       },
     ],
