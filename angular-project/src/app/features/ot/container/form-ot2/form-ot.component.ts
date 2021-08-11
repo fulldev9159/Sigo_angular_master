18711dd721f3f5c7e1f6b350731f18996a36bbe9
Jorge Retamal <jorgeretamalaburto@gmail.com>
2021-08-11 11:47:14 -0400

    Se agrega escoger cubicacion

--- angular-project/src/app/features/ot/container/form-ot2/form-ot.component.ts
+++ angular-project/src/app/features/ot/container/form-ot2/form-ot.component.ts
@@ -12,10 +12,17 @@ import {
   of,
   combineLatest,
 } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { OtFacade } from '@storeOT/features/ot/ot.facade';
+import { CubicacionFacade } from '@storeOT/features/cubicacion/cubicacion.facade';
+import { AuthFacade } from '@storeOT/features/auth/auth.facade';
+
+import { Cubicacion } from '@storeOT/features/cubicacion/cubicacion.model';
+import { Login } from '@data';
 
 @Component({
   selector: 'app-form-ot2',
@@ -25,6 +32,8 @@ import { OtFacade } from '@storeOT/features/ot/ot.facade';
 })
 export class FormOt2Component implements OnInit, OnDestroy {
   subscription: Subscription = new Subscription();
+  public authLogin: Login = null;
+  public cubicaciones$: Observable<Cubicacion[]>;
 
   formControls = {
     nombre: new FormControl('', [
@@ -33,16 +42,51 @@ export class FormOt2Component implements OnInit, OnDestroy {
       Validators.maxLength(100),
     ]),
     tipo: new FormControl(null, [Validators.required]),
+    cubicacion_id: new FormControl(null, [Validators.required]),
   };
 
   formOT: FormGroup = new FormGroup(this.formControls);
 
-  constructor(private router: Router, private otFacade: OtFacade) {}
+  constructor(
+    private router: Router,
+    private otFacade: OtFacade,
+    private cubageFacade: CubicacionFacade,
+    private authFacade: AuthFacade
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.otFacade.resetData();
+    this.initObservables();
+    this.initData();
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  initObservables(): void {
+    this.cubicaciones$ = this.cubageFacade
+      .getCubicacionSelector$()
+      .pipe(
+        map(cubicaciones =>
+          cubicaciones.filter(
+            cubicacion =>
+              !cubicacion.asignado &&
+              this.authLogin.usuario_id === cubicacion.creador_usuario_id
+          )
+        )
+      );
+  }
+
+  initData(): void {
+    this.subscription.add(
+      this.authFacade.getLogin$().subscribe(profile => {
+        if (profile) {
+          this.cubageFacade.getCubicacionAction(+profile.perfiles[0].id);
+          this.authLogin = profile;
+        }
+      })
+    );
   }
 
   goBack(): void {
