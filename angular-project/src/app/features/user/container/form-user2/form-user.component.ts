243d8e7d3523a435142233d153cbdaa3051db110
Jorge Retamal <jorgeretamalaburto@gmail.com>
2021-08-19 17:20:23 -0400

    Se agrega username y se mejora mensaje de select

--- angular-project/src/app/features/user/container/form-user2/form-user.component.ts
+++ angular-project/src/app/features/user/container/form-user2/form-user.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
 } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { UserFacade } from '@storeOT/features/user/user.facade';
 
@@ -15,6 +16,18 @@ import { UserFacade } from '@storeOT/features/user/user.facade';
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormUser2Component implements OnInit, OnDestroy {
+  formControls = {
+    username: new FormControl(null, [Validators.required, this.noWhitespace]),
+    nombres: new FormControl(null, [Validators.required, this.noWhitespace]),
+    apellidos: new FormControl(null, [Validators.required, this.noWhitespace]),
+    rut: new FormControl(null, [Validators.required, this.noWhitespace]),
+    firma: new FormControl(null),
+    calular: new FormControl(null),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+  };
+
+  formUser: FormGroup = new FormGroup(this.formControls);
+
   constructor(private userFacade: UserFacade, private router: Router) {}
   ngOnInit(): void {}
   ngOnDestroy(): void {}
@@ -22,5 +35,11 @@ export class FormUser2Component implements OnInit, OnDestroy {
   goBack(): void {
     this.userFacade.resetData();
     this.router.navigate(['/app/user/list-user']);
+  }
+
+  noWhitespace(control: FormControl): any {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
   }
 }
