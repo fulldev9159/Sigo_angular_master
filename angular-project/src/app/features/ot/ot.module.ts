59ff2442bac53e616a093c43154f239a95475f82
César Agustín González <cesar.gonzalez@zweicom.com>
2021-08-31 12:10:05 -0400

    #14 remove unused components

--- angular-project/src/app/features/ot/ot.module.ts
+++ angular-project/src/app/features/ot/ot.module.ts
@@ -5,12 +5,9 @@ import { OtRoutingModule } from './ot-routing.module';
 import { OtComponent } from './ot.component';
 import { ListOtComponent } from './container/list-ot/list-ot.component';
 import { FormOtComponent } from './container/form-ot/form-ot.component';
-import { FormOt2Component } from './container/form-ot2/form-ot.component';
 
 import { UiModule } from '@uiOT/ui.module';
-import { FormComponent } from './component/form/form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DetailComponent } from './component/detail/detail.component';
 import { CDetalleOtComponent } from './container/c-detalle-ot/c-detalle-ot.component';
 import { DetalleOtComponent } from './component/detalle-ot/detalle-ot.component';
 import { AssignCoordinatorFormComponent } from './component/assign-coordinator-form/assign-coordinator-form.component';
@@ -29,9 +26,6 @@ import { PlanProyectoDetailComponent } from './component/plan-proyecto-detail/pl
     OtComponent,
     ListOtComponent,
     FormOtComponent,
-    FormOt2Component,
-    FormComponent,
-    DetailComponent,
     CDetalleOtComponent,
     DetalleOtComponent,
     AssignCoordinatorFormComponent,
