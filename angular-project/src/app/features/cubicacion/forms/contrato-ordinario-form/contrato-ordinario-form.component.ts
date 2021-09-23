8b317b8be4afc84cf0f5598cc80fdb7a6e14300a
César Agustín González <cesar.gonzalez@zweicom.com>
2021-09-23 17:19:49 -0300

    #17 create alternative table for contrato ordianrio

--- angular-project/src/app/features/cubicacion/forms/contrato-ordinario-form/contrato-ordinario-form.component.ts
+++ angular-project/src/app/features/cubicacion/forms/contrato-ordinario-form/contrato-ordinario-form.component.ts
@@ -21,7 +21,7 @@ import { GeneralFormService } from '../../service/general-form.service';
 import { TipoMonedaFacade } from '@storeOT/features/tipo-moneda/tipo-moneda.facade';
 import { UnidadFacade } from '@storeOT/features/unidad/unidad.facade';
 import { CubicacionFacade } from '@storeOT/features/cubicacion/cubicacion.facade';
-import { TableComponent } from '@uiOT/table/table.component';
+import { ContratoOrdinarioLpusTableComponent } from '@uiOT/contrato-ordinario-lpus-table/contrato-ordinario-lpus-table.component';
 import { Unidad, TipoMoneda } from '@data';
 import { CubicacionWithLpu } from '@data';
 
@@ -57,216 +57,14 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
   unidades$: Observable<Unidad[]> = of([]);
   unidades: Unidad[] = []; // TODO: mejorar ésto
 
-  msgsLPUQuantityZero = [
-    {
-      severity: 'warn',
-      summary: 'ATENCION',
-      detail: 'Hay LPUS con cantidad igual a 0 o inferior',
-    },
-  ];
-
-  msgsLPUSQuantity = [
-    {
-      severity: 'info',
-      // summary: 'ATENCION',
-      detail: 'Al menos 1 LPU debe ser ingresada',
-    },
-  ];
-
-  cumulativeItems = {};
-  ordinaryFieldDebouncer$ = new BehaviorSubject<{
-    header: string;
-    rowIndex: number;
-    value: string;
-    item: CartItem;
-  }>(null);
-  updatingTables = false;
-  tableValid = true;
   lpusCarrito: CartItem[] = [];
-  hasLPUWithZeroQuantity = false;
-  total = 0;
   currency = '';
 
   @ViewChild('tableLpus', {
-    read: TableComponent,
+    read: ContratoOrdinarioLpusTableComponent,
     static: false,
   })
-  tableLpus: TableComponent;
-
-  tableConfiguration = {
-    header: true,
-    headerConfig: {
-      title: '',
-      searchText: 'Filtrar...',
-      paginator: false,
-      actionsType: 'Buttons',
-    },
-    body: {
-      headers: [
-        {
-          field: 'Servicio LPU',
-          type: 'INPUT',
-          sort: 'lpu_nombre',
-          header: 'lpu_nombre',
-          // width: '33%',
-          editable: true,
-          onchange: (event: any, item: CartItem) => {
-            this.updatingTables = true;
-            this.ordinaryFieldDebouncer$.next({
-              header: event.target.header,
-              rowIndex: +event.target.rowIndex,
-              value: event.target.value,
-              item,
-            });
-          },
-          validators: [
-            Validators.required,
-            this.noWhitespace,
-            Validators.maxLength(100),
-          ],
-          errorMessageFn: errors => {
-            if (errors.required) {
-              return 'Este campo es requerido';
-            } else if (errors.whitespace) {
-              return 'Este campo es requerido';
-            } else if (errors.maxlength) {
-              return `Debe tener a lo más ${errors.maxlength.requiredLength} caracteres`;
-            }
-            return 'Este campo es inválido';
-          },
-        },
-        {
-          field: 'Unidad	',
-          type: 'SELECT',
-          sort: 'lpu_unidad_codigo',
-          header: 'lpu_unidad_codigo',
-          editable: true,
-          optionsFn: (item: CartItem) => {
-            return this.unidades.map(unidad => ({
-              id: unidad.id,
-              name: unidad.nombre,
-            }));
-          },
-          onchange: (event: any, item: CartItem) => {
-            this.updatingTables = true;
-            this.ordinaryFieldDebouncer$.next({
-              header: event.target.header,
-              rowIndex: +event.target.rowIndex,
-              value: event.target.value,
-              item,
-            });
-          },
-          validators: [Validators.required],
-          errorMessageFn: errors => {
-            if (errors.required) {
-              return 'Este campo es requerido';
-            }
-            return 'Este campo es inválido';
-          },
-        },
-        {
-          field: 'Cantidad	',
-          type: 'INPUTNUMBER',
-          sort: 'cantidad',
-          header: 'cantidad',
-          editable: true,
-          onchange: (event: any, item: CartItem) => {
-            this.updatingTables = true;
-            this.ordinaryFieldDebouncer$.next({
-              header: event.target.header,
-              rowIndex: +event.target.rowIndex,
-              value: event.target.value,
-              item,
-            });
-          },
-          validators: [
-            Validators.required,
-            this.noWhitespace,
-            this.nonZero,
-            Validators.maxLength(6),
-          ],
-          errorMessageFn: errors => {
-            if (errors.required) {
-              return 'Este campo es requerido';
-            } else if (errors.whitespace) {
-              return 'Este campo es requerido';
-            } else if (errors.nonzero) {
-              return 'No son permitidos valores inferiores a 1';
-            } else if (errors.maxlength) {
-              return `Debe tener a lo más ${errors.maxlength.requiredLength} caracteres`;
-            }
-            return 'Este campo es inválido';
-          },
-        },
-        {
-          field: 'Precio	',
-          type: 'INPUTNUMBER',
-          sort: 'lpu_precio',
-          header: 'lpu_precio',
-          editable: true,
-          onchange: (event: any, item: CartItem) => {
-            this.updatingTables = true;
-            this.ordinaryFieldDebouncer$.next({
-              header: event.target.header,
-              rowIndex: +event.target.rowIndex,
-              value: event.target.value,
-              item,
-            });
-          },
-          validators: [
-            Validators.required,
-            this.noWhitespace,
-            this.nonZero,
-            Validators.maxLength(6),
-          ],
-          errorMessageFn: errors => {
-            if (errors.required) {
-              return 'Este campo es requerido';
-            } else if (errors.whitespace) {
-              return 'Este campo es requerido';
-            } else if (errors.nonzero) {
-              return 'No son permitidos valores inferiores a 1';
-            } else if (errors.maxlength) {
-              return `Debe tener a lo más ${errors.maxlength.requiredLength} caracteres`;
-            }
-            return 'Este campo es inválido';
-          },
-        },
-        {
-          field: 'Tipo Moneda	',
-          type: 'TEXT',
-          sort: 'tipo_moneda_cod',
-          header: 'tipo_moneda_cod',
-          editable: false,
-        },
-        {
-          field: 'Subtotal	',
-          type: 'NUMBER',
-          sort: 'lpu_subtotal',
-          header: 'lpu_subtotal',
-        },
-        {
-          field: null,
-          type: 'ACTIONS',
-          sort: null,
-          header: null,
-          editable: false,
-        },
-      ],
-      sort: ['lpu_nombre'],
-      actions: [
-        {
-          icon: 'p-button-icon pi pi-trash',
-          class: 'p-button-rounded p-button-danger',
-          onClick: (event: Event, item: CartItem, rowIndex: number) => {
-            if (!this.updatingTables) {
-              this.deleteCartItem(item, rowIndex);
-            }
-          },
-        },
-      ],
-    },
-  }; // tslint:disable-line
+  tableLpus: ContratoOrdinarioLpusTableComponent;
 
   controls = {
     tipo_moneda_id: new FormControl(null, [Validators.required]),
@@ -321,13 +119,7 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
 
     this.unidades$ = this.unidadFacade.getUnidades$().pipe(
       map(unidades => unidades || []),
-      map(unidades => (this.unidades = unidades))
-    );
-    this.subscription.add(
-      this.unidades$.subscribe(
-        unidades => (this.unidades = unidades),
-        err => (this.unidades = [])
-      )
+      tap(unidades => (this.unidades = unidades))
     );
 
     this.subscription.add(
@@ -406,48 +198,6 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
       )
     );
 
-    this.subscription.add(
-      this.ordinaryFieldDebouncer$
-        .pipe(
-          filter(target => target !== null),
-          tap(({ header, rowIndex, value, item }) => {
-            if (this.cumulativeItems[rowIndex + ''] === undefined) {
-              this.cumulativeItems[rowIndex + ''] = {};
-            }
-
-            switch (header) {
-              case 'lpu_unidad_codigo':
-                const lpu_unidad_codigo = (val => (isNaN(val) ? 0 : val))(
-                  parseInt(value, 10)
-                );
-                const unidad = this.unidades.find(
-                  u => u.id === lpu_unidad_codigo
-                );
-                if (unidad) {
-                  const key = 'lpu_unidad_nombre';
-                  this.cumulativeItems[rowIndex + ''][key] = unidad.nombre;
-                }
-                this.cumulativeItems[rowIndex + ''][header] = lpu_unidad_codigo;
-                break;
-              case 'cantidad':
-              case 'lpu_precio':
-                const numericValue = (val => (isNaN(val) ? 0 : val))(
-                  parseInt(value, 10)
-                );
-                this.cumulativeItems[rowIndex + ''][header] = numericValue;
-                break;
-              case 'lpu_nombre':
-                this.cumulativeItems[rowIndex + ''][header] = value;
-                break;
-            }
-          }),
-          debounceTime(800)
-        )
-        .subscribe(({ header, rowIndex, value, item }) =>
-          this.ordinaryLpuFieldChanged(header, rowIndex, value, item)
-        )
-    );
-
     this.form.get('tipo_moneda_id').disable({ emitEvent: false });
 
     this.selectedCubicacion$ = this.cubageFacade
@@ -480,9 +230,9 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
           this.form.get('descripcion').setValue('No tiene descripcion');
 
           if (tipoMoneda) {
-            this.form.get('tipo_moneda_id').setValue(tipoMoneda.id);
-
             setTimeout(() => {
+              this.form.get('tipo_moneda_id').setValue(tipoMoneda.id);
+
               this.lpusCarrito = cubicacion.lpus.map(lpu => ({
                 lpu_nombre: lpu.lpu_nombre,
                 lpu_unidad_codigo: lpu.tipo_unidad_codigo,
@@ -494,10 +244,9 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
                 lpu_subtotal: lpu.lpu_subtotal,
               }));
 
-              this.updateLpusTableInformation();
               this.initializationFinished$.next(true);
               this.detector.detectChanges();
-            }, 1000);
+            }, 0);
           } else {
             this.initializationFinished$.next(true);
             this.incompleteCubicacionError$ = of(
@@ -512,7 +261,6 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
     this.initializationFinished$.next(true);
     this.initializationFinished$.complete();
-    // this.cantidadDebouncer$.complete();
   }
 
   resetTiposMonedaFormControl(): void {
@@ -527,124 +275,41 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
     }
   }
 
-  ordinaryLpuFieldChanged(
-    header: string,
-    rowIndex: number,
-    value: string,
-    item: CartItem
-  ): void {
-    Object.keys(this.cumulativeItems).forEach(index => {
-      Object.keys(this.cumulativeItems[index]).forEach(field => {
-        this.lpusCarrito[+index][field] = this.cumulativeItems[index][field];
-      });
-    });
-    this.cumulativeItems = {};
-    this.lpusCarrito = this.lpusCarrito.map((x, index) => ({
-      ...x,
-      lpu_subtotal: +x.lpu_precio * +x.cantidad,
-    }));
-
-    this.tableValid = this.tableLpus.valid;
-    this.updatingTables = false;
-    this.updateLpusTableInformation();
-    this.detector.detectChanges();
-    this.valueChanges.emit({
-      controlName: 'lpus',
-      value: {
-        lpus: this.lpusCarrito,
-      },
-    });
-  }
-
-  deleteCartItem(item: CartItem, rowIndex): void {
-    const tmp = [...this.lpusCarrito];
-    tmp.splice(rowIndex, 1);
-    this.lpusCarrito = [...tmp];
-
-    this.tableValid = this.tableLpus.valid;
-    this.updateLpusTableInformation();
-    this.detector.detectChanges();
-    this.valueChanges.emit({
-      controlName: 'lpus',
-      value: {
-        lpus: this.lpusCarrito,
-      },
-    });
-  }
-
   resetLpusCarrito(): void {
-    this.lpusCarrito = [];
-    this.updateLpusTableInformation();
+    this.tableLpusReset();
     this.valueChanges.emit({
       controlName: 'lpus',
       value: {
-        lpus: this.lpusCarrito,
+        lpus: [],
       },
     });
-  }
-
-  updateLpusTableInformation(): void {
-    this.updateTotal();
-    // this.updateCurrencyNormal();
-    this.validateLpusWithZeroQuantiy();
   }
 
   updateCurrency(currency: string): void {
     this.currency = currency;
   }
 
-  updateTotal(): void {
-    this.total = this.lpusCarrito.reduce((total, currentValue) => {
-      return total + currentValue.lpu_subtotal;
-    }, 0);
-  }
-
-  validateLpusWithZeroQuantiy(): void {
-    const item = this.lpusCarrito.find(lpu => lpu.cantidad < 1);
-    this.hasLPUWithZeroQuantity = item !== undefined;
-  }
-
   notAllowedToAddRow(): boolean {
     const { tipo_moneda_id } = this.form.getRawValue();
-    return !(
-      tipo_moneda_id !== null &&
-      !this.hasLPUWithZeroQuantity &&
-      this.tableValid &&
-      !this.updatingTables
-    );
+    const items = this.tableLpusValues;
+    const canAdd =
+      tipo_moneda_id !== null && (items.length === 0 || this.tableLpusValid);
+    return !canAdd;
   }
 
   addTableRow(): void {
     const { tipo_moneda_id, tipo_moneda_cod } = this.form.getRawValue();
 
-    this.lpusCarrito = [
-      ...this.lpusCarrito,
-      {
-        lpu_nombre: 'Nuevo',
-        lpu_unidad_codigo:
-          this.unidades.length > 0 ? this.unidades[0].id : null,
-        lpu_unidad_nombre:
-          this.unidades.length > 0 ? this.unidades[0].nombre : '',
-        cantidad: 1,
-        lpu_precio: 1,
-        tipo_moneda_cod,
-        tipo_moneda_id,
-        lpu_subtotal: 0,
-      },
-    ];
-    this.lpusCarrito = this.lpusCarrito.map((x, index) => ({
-      ...x,
-      lpu_subtotal: +x.lpu_precio * +x.cantidad,
-    }));
-
-    this.tableValid = this.tableLpus.valid;
-    this.updateLpusTableInformation();
-    this.detector.detectChanges();
-    this.valueChanges.emit({
-      controlName: 'lpus',
-      value: {
-        lpus: this.lpusCarrito,
-      },
+    this.tableLpusAddItem({
+      lpu_nombre: '',
+      lpu_unidad_codigo: this.unidades.length > 0 ? this.unidades[0].id : null,
+      lpu_unidad_nombre:
+        this.unidades.length > 0 ? this.unidades[0].nombre : '',
+      cantidad: 0,
+      lpu_precio: 0,
+      tipo_moneda_cod,
+      tipo_moneda_id,
+      lpu_subtotal: 0,
     });
   }
 
@@ -660,28 +325,108 @@ export class ContratoOrdinarioFormComponent implements OnInit, OnDestroy {
       onlySelf: true,
     });
 
-    this.tableLpus.touch();
+    this.tableLpusTouch();
   }
 
   get valid(): boolean {
-    return (
-      this.form.valid &&
-      this.lpusCarrito.length > 0 &&
-      !this.hasLPUWithZeroQuantity &&
-      this.tableValid &&
-      !this.updatingTables
-    );
+    return this.form.valid && this.tableLpusValid;
+  }
+
+  tableLpusAddItem(item: CartItem): void {
+    if (this.tableLpus !== undefined) {
+      this.tableLpus.addItem(item);
+    }
+  }
+
+  tableLpusReset(): void {
+    if (this.tableLpus !== undefined) {
+      this.tableLpus.reset();
+    }
+  }
+
+  tableLpusTouch(): void {
+    if (this.tableLpus !== undefined) {
+      this.tableLpus.touch();
+    }
+  }
+
+  get tableLpusValid(): boolean {
+    if (this.tableLpus === undefined) {
+      return false;
+    }
+    return this.tableLpus.valid;
+  }
+
+  get tableLpusValues(): CartItem[] {
+    if (this.tableLpus === undefined) {
+      return [];
+    }
+    return this.tableLpus.values;
+  }
+
+  itemAdded({ item }): void {
+    const items = this.tableLpusValues;
+    this.detector.detectChanges();
+    this.valueChanges.emit({
+      controlName: 'lpus',
+      value: {
+        lpus: items,
+      },
+    });
+  }
+
+  itemDeleted({ item }): void {
+    const items = this.tableLpusValues;
+    this.detector.detectChanges();
+    this.valueChanges.emit({
+      controlName: 'lpus',
+      value: {
+        lpus: items,
+      },
+    });
+  }
+
+  cellValueChanged({ cantidad, item }): void {
+    const items = this.tableLpusValues;
+    this.detector.detectChanges();
+    this.valueChanges.emit({
+      controlName: 'lpus',
+      value: {
+        lpus: items,
+      },
+    });
   }
 
   get values(): any {
     const { tipo_moneda_id, tipo_moneda_cod, descripcion } =
       this.form.getRawValue();
+    const items = this.tableLpusValues;
 
     return {
       tipo_moneda_id: +tipo_moneda_id,
       tipo_moneda_cod,
       descripcion,
-      lpus: this.lpusCarrito,
+      lpus: items.map(
+        ({
+          lpu_nombre,
+          lpu_unidad_codigo,
+          lpu_unidad_nombre,
+          cantidad,
+          lpu_precio,
+          tipo_moneda_cod,
+          tipo_moneda_id,
+          lpu_subtotal,
+        }) => ({
+          lpu_nombre,
+          lpu_unidad_codigo,
+          lpu_unidad_nombre,
+          cantidad,
+          lpu_precio,
+          tipo_moneda_cod,
+          tipo_moneda_id,
+          lpu_subtotal,
+        })
+      ),
     };
   }
 }
