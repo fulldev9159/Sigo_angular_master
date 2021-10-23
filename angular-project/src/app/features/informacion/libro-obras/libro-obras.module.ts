f8e29fef88a6485315f422742a9f73fcf855252e
Jorge Retamal <jorgeretamalaburto@gmail.com>
2021-10-23 00:58:17 -0300

    Migrado libro de obras

--- angular-project/src/app/features/informacion/libro-obras/libro-obras.module.ts
+++ angular-project/src/app/features/informacion/libro-obras/libro-obras.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LibroObrasRoutingModule } from './libro-obras-routing.module';
 import { LibroObrasComponent } from './libro-obras.component';
-
+import { UiModule } from '@uiOT/ui.module';
 
 @NgModule({
   declarations: [LibroObrasComponent],
-  imports: [
-    CommonModule,
-    LibroObrasRoutingModule
-  ]
+  imports: [CommonModule, LibroObrasRoutingModule, UiModule],
 })
-export class LibroObrasModule { }
+export class LibroObrasModule {}
