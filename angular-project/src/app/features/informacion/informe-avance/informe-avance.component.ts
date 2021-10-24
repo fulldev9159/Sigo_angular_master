d31f94bcc0b66825a5dcc491ed4b4d6507fb769c
Jorge Retamal <jorgeretamalaburto@gmail.com>
2021-10-24 18:17:19 -0300

    se crea informe de avances

--- angular-project/src/app/features/informacion/informe-avance/informe-avance.component.ts
+++ angular-project/src/app/features/informacion/informe-avance/informe-avance.component.ts
@@ -9,6 +9,73 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class InformeAvanceComponent implements OnInit, OnDestroy {
   subscription: Subscription = new Subscription();
+  data = [
+    {
+      cor: 10,
+      direccion: 'APOQUINDO - ESTEBAN DEL ORTO',
+      clave: 'C022',
+      codigo: 'J770',
+      descripcion: 'PREPARAR TUBO/S CABLE DE F.O(EN PUNTA O CON SANGRADO)',
+      cub: 2,
+    },
+    {
+      cor: 20,
+      direccion: 'APOQUINDO - ESTEBAN DEL ORTO',
+      clave: 'C002',
+      codigo: 'J765',
+      descripcion: 'MANUPULAR CABLE O ELEMENTO DE F.O (EMR., TERM.,ASOC.)',
+      cub: 1,
+    },
+    {
+      cor: 30,
+      direccion: 'APOQUINDO - ESTEBAN DEL ORTO',
+      clave: 'C002',
+      codigo: 'J791',
+      descripcion: 'EMPALMAR MODULO DE 4 F.O.',
+      cub: 1,
+    },
+    {
+      cor: 40,
+      direccion: 'APOQUINDO - ESTEBAN DEL ORTO',
+      clave: 'C002',
+      codigo: 'J158',
+      descripcion: 'INSTALAR AMARRAS PLASTICAS DE TODO TIPO',
+      cub: 5,
+    },
+    {
+      cor: 50,
+      direccion: 'APOQUINDO - MANQUEHUE',
+      clave: 'C022',
+      codigo: 'J770',
+      descripcion: 'PREPARAR TUBO/S CABLE DE F.O(EN PUNTA O CON SANGRADO)',
+      cub: 2,
+    },
+
+    {
+      cor: 60,
+      direccion: 'APOQUINDO - MANQUEHUE',
+      clave: 'C002',
+      codigo: 'J765',
+      descripcion: 'MANUPULAR CABLE O ELEMENTO DE F.O (EMR., TERM.,ASOC.)',
+      cub: 1,
+    },
+    {
+      cor: 30,
+      direccion: 'APOQUINDO - MANQUEHUE',
+      clave: 'C002',
+      codigo: 'J791',
+      descripcion: 'EMPALMAR MODULO DE 4 F.O.',
+      cub: 1,
+    },
+    {
+      cor: 40,
+      direccion: 'APOQUINDO - MANQUEHUE',
+      clave: 'C002',
+      codigo: 'J158',
+      descripcion: 'INSTALAR AMARRAS PLASTICAS DE TODO TIPO',
+      cub: 5,
+    },
+  ];
 
   constructor(private rutaActiva: ActivatedRoute) {}
 
