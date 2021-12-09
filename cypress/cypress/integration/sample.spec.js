de3a90a61035bc8da1c2741131951a7a76edc469
Jorge Retamal <jorgeretamalaburto@gmail.com>
2021-12-09 15:31:57 -0300

    cypress test start 2

--- cypress/cypress/integration/sample.spec.js
+++ cypress/cypress/integration/sample.spec.js
@@ -4,7 +4,7 @@ describe('My First Test', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://34.122.92.38:4300/app/dashboard');
+    cy.visit('http://localhost:4201/app/dashboard');
   });
   it('Does not do much!', () => {
     cy.get('#text').type('mgestor1');
