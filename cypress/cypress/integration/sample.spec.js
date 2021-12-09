05f41229367465877303620c7200a82d1ce4d578
Jorge Retamal <jorgeretamalaburto@gmail.com>
2021-12-08 21:15:08 -0300

    cypress test

---
+++ cypress/cypress/integration/sample.spec.js
@@ -0,0 +1,14 @@
+describe('My First Test', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://34.122.92.38:4300/app/dashboard');
+  });
+  it('Does not do much!', () => {
+    cy.get('#text').type('mgestor1');
+    cy.get('#password').type('M0v15tar.{enter}');
+    expect(true).to.equal(true);
+  });
+});
