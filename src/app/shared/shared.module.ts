e87f606a0ca24cce7922728a8547496c208e4a15
Jorge R <jorge.retamal@zweicom.com>
2022-07-24 18:17:25 -0400

    primeng

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PrimeNgModule } from './primeng/primeng.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, PrimeNgModule],
+  exports: [PrimeNgModule],
 })
-export class SharedModule { }
+export class SharedModule {}
