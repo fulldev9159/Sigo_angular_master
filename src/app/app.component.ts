9a9aa8ab8035c8a80d28a4fc73f4e63465bf2d3e
Jorge R <jorge.retamal@zweicom.com>
2022-07-24 13:29:35 -0400

    center login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'web-sigo';
+export class AppComponent implements OnInit {
+  constructor(private primeConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primeConfig.ripple = true;
+  }
 }
