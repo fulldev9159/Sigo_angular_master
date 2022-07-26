00829c96e61217d1a6ada2d10803bb82ae9281f2
Jorge R <jorge.retamal@zweicom.com>
2022-07-26 09:48:58 -0400

    auth css

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -1,4 +1,8 @@
 import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
-@NgModule({ imports: [InputTextModule], exports: [InputTextModule] })
+import { CardModule } from 'primeng/card';
+@NgModule({
+  imports: [InputTextModule, CardModule],
+  exports: [InputTextModule, CardModule],
+})
 export class PrimeNgModule {}
