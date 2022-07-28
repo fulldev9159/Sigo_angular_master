958fbe8749ea2916e2e7d96cd2345673c80d12ce
Jorge R <jorge.retamal@zweicom.com>
2022-07-28 19:07:38 -0400

    create action login #26

---
+++ src/app/store/auth/auth.actions.ts
@@ -0,0 +1,17 @@
+import { Login, Response } from '@model';
+import { createAction, props } from '@ngrx/store';
+
+export const login = createAction(
+  '[AUTH] POST Login',
+  props<{ username: string; password: string }>()
+);
+
+export const loginSuccess = createAction(
+  '[AUTH] POST Login Success',
+  props<{ response: Response<Login> }>()
+);
+
+export const loginError = createAction(
+  '[AUTH] POST Login Error',
+  props<{ error: any }>()
+);
