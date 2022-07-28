082666e4ef35e6c5559dcc726d504fc951d02ee1
Jorge R <jorge.retamal@zweicom.com>
2022-07-28 19:19:11 -0400

    create effect login #26

--- src/app/store/auth/auth.effects.ts
+++ src/app/store/auth/auth.effects.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { LoginService } from '@services';
+import * as authActions from './auth.actions';
+import { catchError, concatMap, map } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 @Injectable()
-export class AuthEffects {}
+export class AuthEffects {
+  constructor(private actions$: Actions, private loginService: LoginService) {}
+
+  Login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(authActions.login),
+      concatMap(({ username, password }) =>
+        this.loginService.logIn(username, password).pipe(
+          map(response => authActions.loginSuccess({ response })),
+          catchError(error => of(authActions.loginError({ error })))
+        )
+      )
+    )
+  );
+}
