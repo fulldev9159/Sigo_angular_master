f9928419704ea82f9aa7c43c09bf722506df69e2
Jorge R <jorge.retamal@zweicom.com>
2022-07-27 22:36:59 -0400

    test login service #26

--- src/app/core/service/login.service.spec.ts
+++ src/app/core/service/login.service.spec.ts
@@ -1,27 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoginService } from './login.service';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
+import { LoginMock } from '@mocksOT';
+import { of } from 'rxjs';
 
 describe('LoginService', () => {
   let service: LoginService;
-  let httpController: HttpTestingController;
-  let httpClient: HttpClient;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
-    service = TestBed.inject(LoginService);
-    httpController = TestBed.inject(HttpTestingController);
-    httpClient = TestBed.inject(HttpClient);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    service = new LoginService(httpClientSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call login and return and login Data', (done: DoneFn) => {
+    httpClientSpy.post.and.returnValue(of(LoginMock));
+    service.logIn('mgestor1', 'asdasd').subscribe({
+      next: response => {
+        expect(response).toEqual(LoginMock);
+        done();
+      },
+      error: done.fail,
+    });
   });
 });
---
+++ src/mocks/login.ts
@@ -0,0 +1,9 @@
+export let LoginMock = {
+  status: { code: 0, desc: 'OK' },
+  data: {
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTg5NzcxNDQsImlzcyI6InNpZ28iLCJuYmYiOjE2NTg5NzM1NDQsInNpZ29fcHJveHlfaWQiOi0xLCJzaWdvX3VzZXJfaWQiOjIsInNpZ29fcGVyZmlsX2lkIjotMX0.P59Wb-rm-bSS-Wc4geOchYlzAfZQZH0Hvqu_O292-dk',
+    usuario_id: 2,
+    usuario_nombre: 'JESSICA MOVISTAR CASTILLO 1',
+  },
+};
