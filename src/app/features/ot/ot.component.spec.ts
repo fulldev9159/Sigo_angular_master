0d2e13e8a6143abf04c5776b70a3975fe9e582ff
Jorge R <jorge.retamal@zweicom.com>
2022-08-30 21:32:19 -0400

    unit test fix

--- src/app/features/ot/ot.component.spec.ts
+++ src/app/features/ot/ot.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OtComponent } from './ot.component';
@@ -8,9 +9,9 @@ describe('OtComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OtComponent ]
-    })
-    .compileComponents();
+      declarations: [OtComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(OtComponent);
     component = fixture.componentInstance;
