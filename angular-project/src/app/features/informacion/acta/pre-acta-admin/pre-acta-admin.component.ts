8bace2bf91ea904866bbe1e00eb622fef883ff47
Jorge Retamal <jorgeretamalaburto@gmail.com>
2022-09-02 13:45:00 -0400

    Despliegue de datos nueva acta

--- angular-project/src/app/features/informacion/acta/pre-acta-admin/pre-acta-admin.component.ts
+++ angular-project/src/app/features/informacion/acta/pre-acta-admin/pre-acta-admin.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { DetalleActaServicio, DetalleActaUob } from '@data';
+import { OtFacade } from '@storeOT/index';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pre-acta-admin',
   templateUrl: './pre-acta-admin.component.html',
-  styleUrls: ['./pre-acta-admin.component.scss']
+  styleUrls: ['./pre-acta-admin.component.scss'],
 })
 export class PreActaAdminComponent implements OnInit {
+  subscription: Subscription = new Subscription();
 
-  constructor() { }
+  detalleActa$: Observable<{
+    ultimo_tipo_pago: string;
+    servicios: DetalleActaServicio[];
+    unidades_obra: DetalleActaUob[];
+  }> = this.otFacade.getDetalleActa$();
+  ot$: Observable<any> = this.otFacade.getDetalleOT$();
+  saving$: Observable<boolean> = this.otFacade.sendingGeneracionActa$();
+  totalServicios: number;
+  totalUO: number;
+  totalServicios_servicio: number;
+  totalUO_servicio: number;
+  constructor(private otFacade: OtFacade) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
