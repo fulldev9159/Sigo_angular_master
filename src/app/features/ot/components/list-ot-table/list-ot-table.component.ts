a058882f6f6ab13dd43d6a9ab38acc1d1d632b54
Jorge R <jorge.retamal@zweicom.com>
2022-09-19 02:18:02 -0300

    table ot

--- src/app/features/ot/components/list-ot-table/list-ot-table.component.ts
+++ src/app/features/ot/components/list-ot-table/list-ot-table.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import { OT } from '@model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'zwc-list-ot-table',
   templateUrl: './list-ot-table.component.html',
-  styleUrls: ['./list-ot-table.component.scss']
+  styleUrls: ['./list-ot-table.component.scss'],
 })
-export class ListOtTableComponent implements OnInit {
+export class ListOtTableComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription();
 
-  constructor() { }
+  @Input() data: OT[];
+
+  filterIcon = faFilter;
+
+  // FORMULARIO
+  formFilterControl = {
+    ot_id: new FormControl('', []),
+    nombre: new FormControl('', []),
+    contrato_marco: new FormControl(''),
+    proveedor: new FormControl(''),
+    gestor: new FormControl(''),
+  };
+
+  formFilter: FormGroup = new FormGroup(this.formFilterControl);
+
+  constructor() {}
 
   ngOnInit(): void {
+    console.log();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
