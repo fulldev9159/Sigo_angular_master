7da0e01fbfbd12da4ae5c53420bce80315df61d6
Jorge R <jorge.retamal@zweicom.com>
2022-09-24 19:57:09 -0300

    fix test

--- src/app/features/ot-detalle/components/informe-avance/informe-avance.component.spec.ts
+++ src/app/features/ot-detalle/components/informe-avance/informe-avance.component.spec.ts
@@ -1,4 +1,7 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
 
 import { InformeAvanceComponent } from './informe-avance.component';
 
@@ -8,9 +11,10 @@ describe('InformeAvanceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InformeAvanceComponent ]
-    })
-    .compileComponents();
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [StoreModule.forRoot({}), RouterTestingModule],
+      declarations: [InformeAvanceComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InformeAvanceComponent);
     component = fixture.componentInstance;
