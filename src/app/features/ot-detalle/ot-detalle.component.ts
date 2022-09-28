b2d0a4a9a58f8f9bceb7b71edaa6a6cde8df2942
Jorge R <jorge.retamal@zweicom.com>
2022-09-28 15:41:20 -0300

    Cambios en informe avance

--- src/app/features/ot-detalle/ot-detalle.component.ts
+++ src/app/features/ot-detalle/ot-detalle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthFacade } from '@storeOT/auth/auth.facades';
 
+// TODO: VER MANERA DE OBTENER LAS ACCIONES DE LA OT
 @Component({
   selector: 'zwc-ot-detalle',
   templateUrl: './ot-detalle.component.html',
