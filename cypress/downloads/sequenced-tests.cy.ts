8b72c58484e89e683ca3c8855cf19e0dd059f359
Jorge R <jorge.retamal@zweicom.com>
2022-10-01 02:08:44 -0300

    test fix

--- cypress/downloads/sequenced-tests.cy.ts
+++ cypress/downloads/sequenced-tests.cy.ts
@@ -1,4 +1,4 @@
-import './01_login.cy';
+import '../e2e/01_login.cy';
 import './02_perfil-select.cy';
 import './03_home.cy';
-import './04_cubicacion/04_01_formulario.cy';
+import '../e2e/04_01_formulario.cy';
