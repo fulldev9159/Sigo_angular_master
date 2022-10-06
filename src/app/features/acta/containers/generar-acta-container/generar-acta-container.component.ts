4adf80bc1dee61ead4a67a9819fded45699da6a1
Jorge R <jorge.retamal@zweicom.com>
2022-10-06 12:40:52 -0300

    TODOS

--- src/app/features/acta/containers/generar-acta-container/generar-acta-container.component.ts
+++ src/app/features/acta/containers/generar-acta-container/generar-acta-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 // TODO: CONFIRMAR SI SOLO SE VAN A UTILIZAR LOS ENDPOINTS DE GET SERVICIOS FOR ACTA Y GET UOS FOR ACTA
-
+// TODO: GENERAR REGLAS PARA DETERMINAR EN QUE CONDICION ESTÁ EL SERVICIO Y LA UO
 @Component({
   selector: 'zwc-generar-acta-container',
   templateUrl: './generar-acta-container.component.html',
