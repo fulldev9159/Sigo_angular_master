204a37013866af3a64ba7ad99f6687ae2c452b96
Jorge R <jorge.retamal@zweicom.com>
2022-10-27 18:36:49 -0300

    preparando el ambiente

--- src/app/features/acta/containers/list-actas-container/list-actas-container.component.ts
+++ src/app/features/acta/containers/list-actas-container/list-actas-container.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActaFacade } from '@storeOT/acta/acta.facades';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'zwc-list-actas-container',
   templateUrl: './list-actas-container.component.html',
   styleUrls: ['./list-actas-container.component.scss'],
 })
-export class ListActasContainerComponent implements OnInit {
+export class ListActasContainerComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription();
+
   constructor(private actaFacade: ActaFacade) {}
 
   ngOnInit(): void {
     this.actaFacade.getDetalleActa(1);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
