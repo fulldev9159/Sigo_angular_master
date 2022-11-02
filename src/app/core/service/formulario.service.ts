66fd045f34f97301f1c87c6727679f878d530a4b
Jorge R <jorge.retamal@zweicom.com>
2022-11-02 18:19:41 -0300

    look and feel por servicios done #223

--- src/app/core/service/formulario.service.ts
+++ src/app/core/service/formulario.service.ts
@@ -20,6 +20,14 @@ export class FormularioService {
       return 'Este campo es requerido';
     } else if (errors.minlength) {
       return `El texto debe tener un tamaño mínimo de ${errors.minlength.requiredLength} caracteres`;
+    } else if (errors.benumber) {
+      return 'Debe ser un número';
+    } else if (errors.nonzero) {
+      return 'No son permitidos valores inferiores a 1';
+    } else if (errors.max) {
+      return `Valor no debe ser mayor a ${errors.max.max}`;
+    } else if (errors.min) {
+      return `No puede ser negativo`;
     }
     return 'Error sin mensaje definido';
   }
