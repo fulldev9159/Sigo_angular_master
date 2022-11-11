b2be72e0283261fefebcb3424aeb5a6c53c4a18b
Jorge R <jorge.retamal@zweicom.com>
2022-11-10 22:40:46 -0300

    finalización del ver actas
    Closes #258 #253 #249 #242 #241

--- src/app/features/acta/containers/list-actas-container/list-actas-container.component.ts
+++ src/app/features/acta/containers/list-actas-container/list-actas-container.component.ts
@@ -22,6 +22,7 @@ export class ListActasContainerComponent implements OnInit, OnDestroy {
   actaIdentify: number = 0;
   titleArray: { [key: string]: string };
   API_URL = '';
+  actaSelected: listarActa;
 
   constructor(
     private actaFacade: ActaFacade,
@@ -41,13 +42,15 @@ export class ListActasContainerComponent implements OnInit, OnDestroy {
     this.titleArray = {
       VALIDACION_ACTA: 'Validacion del Acta',
       INF_TRAB_FIN_ACTA: 'Informe de trabajos finalizados',
+      AUTORIZACION_PAGO: 'Autorización de pago',
+      PRIMERA_INTEGRACION_PAGO: 'Envío a imputación',
     };
 
     this.authFacade.showMenuDetalleOT(true);
   }
 
-  OnDetallActa(acta_id: number): void {
-    if (this.actaIdentify != acta_id) this.actaFacade.getDetalleActa(acta_id);
+  OnDetallActa(acta: listarActa): void {
+    if (this.actaIdentify != acta.id) this.actaFacade.getDetalleActa(acta.id);
 
     this.subscription.add(
       this.store
@@ -60,13 +63,15 @@ export class ListActasContainerComponent implements OnInit, OnDestroy {
               items: detallActa?.data?.items?.map(item => ({
                 ...item,
                 observacion: JSON.parse(item.metadata)?.observacion,
+                evente: JSON.parse(item.metadata)?.evento,
               })),
             },
           };
 
-          this.actaIdentify = acta_id;
+          this.actaIdentify = acta.id;
         })
     );
+    this.actaSelected = acta;
   }
 
   OnDescargarActa(): void {}
