f3d103e321a63b89bc990f2dbea93838abac45e3
Jorge R <jorge.retamal@zweicom.com>
2022-11-23 16:45:49 -0300

    fix test

--- cypress/e2e/06_02_Bucle_OT_ET_AUTORIZACION_PROVEEDOR.cy.ts
+++ cypress/e2e/06_02_Bucle_OT_ET_AUTORIZACION_PROVEEDOR.cy.ts
@@ -5,23 +5,28 @@ describe('OT_ET_AUTORIZACION_PROVEEDOR BUCLE', () => {
     cy.viewport(1500, 1700);
   });
 
-  it('should let enter to web', () => {
+  it('should acept cubicacion', () => {
+    cy.intercept('POST', '/ot/posibles_trabajadores/get').as('HTTPRESPONSE');
+
     cy.visit('http://localhost:4206/login/auth');
     cy._login('ccadmincontrato1', 'asdasd');
     cy._select_profile('Administrador EECC');
     cy.get('button[id="navbar-list-ot"]').click();
-  });
-
-  it('aceptar ot supervisor', () => {
-    cy.intercept('POST', '/ot/posibles_trabajadores/get').as('HTTPRESPONSE');
-    cy.get('button[id="play-button"]').click();
-    cy.wait('@HTTPRESPONSE').then(() => {
-      cy._select_dropdown(
-        '#select-supervisor-trabajos',
-        'Ana COBRA CHILE Antonella'
-      );
-      cy.get('button[id="button-confirmar"]').click();
-      cy.wait(1000);
+    cy.wait(100).then(() => {
+      cy.get('#table-ejecucion>p-table>div>div>table>tbody')
+        .contains('OT Test Bucle Cypress')
+        .siblings()
+        .eq(8)
+        .find('button[id="play-button"]')
+        .click();
+      cy.wait('@HTTPRESPONSE').then(() => {
+        cy._select_dropdown(
+          '#select-supervisor-trabajos',
+          'Ana COBRA CHILE Antonella'
+        );
+        cy.get('button[id="button-confirmar"]').click();
+        cy.wait(1000);
+      });
     });
   });
 });
