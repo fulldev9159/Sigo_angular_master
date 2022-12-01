feb26093588c37b361873ca1e7a8363393de8e3e
Jorge R <jorge.retamal@zweicom.com>
2022-12-01 15:51:28 -0300

    desplegar tabla de validaciones #322

--- src/app/features/ingenieria/containers/validar-ingenieria-container/validar-ingenieria-container.component.ts
+++ src/app/features/ingenieria/containers/validar-ingenieria-container/validar-ingenieria-container.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  Accion,
+  CarritoService,
+  DetalleServicioCubicacion,
+  InfoOT,
+  SessionData,
+} from '@model';
+import { TableServiciosComponent } from '@sharedOT/table-servicios/table-servicios.component';
 import { AuthFacade } from '@storeOT/auth/auth.facades';
 import { Subscription } from 'rxjs';
 
@@ -10,10 +19,114 @@ import { Subscription } from 'rxjs';
 export class ValidarIngenieriaContainerComponent implements OnInit, OnDestroy {
   subscription: Subscription = new Subscription();
 
-  constructor(private authFacade: AuthFacade) {}
+  @ViewChild('tableServiciosAutorizarAdicionales', {
+    read: TableServiciosComponent,
+    static: false,
+  })
+  tableServiciosAutorizarAdicionales: TableServiciosComponent;
+
+  dataOT: InfoOT;
+  dataCubicacionIngenieria: CarritoService[] = [];
+  accionesOT: Accion[] = [];
+  cubicacion_ingeniria_id: number;
+  ot_id: number;
+  contrato: string;
+
+  permisos: string[] = (
+    JSON.parse(localStorage.getItem('auth')).sessionData as SessionData
+  ).permisos.map(value => value.slug);
+
+  constructor(private authFacade: AuthFacade, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.authFacade.showMenuDetalleOT(true);
+    this.subscription.add(
+      this.route.data.subscribe(
+        ({ detalleOT, cubicacion, cubicacionIngenieria, accionesOT }) => {
+          console.log(accionesOT);
+          if (accionesOT) this.accionesOT = accionesOT;
+
+          if (detalleOT) {
+            this.dataOT = detalleOT.data.ot;
+            this.contrato =
+              this.dataOT.model_cubicacion_id.model_contrato_id.model_tipo_contrato_id.nombre;
+            this.cubicacion_ingeniria_id = this.dataOT.cubicacion_ing_id;
+            this.ot_id = this.dataOT.id;
+          }
+          if (
+            cubicacionIngenieria &&
+            cubicacionIngenieria.data.many_cubicacion_has_servicio
+          ) {
+            // console.log(cubicacionIngenieria)
+            cubicacionIngenieria.data.many_cubicacion_has_servicio.forEach(
+              (service: DetalleServicioCubicacion) => {
+                service.many_cubicacion_has_uob.forEach(uo => {
+                  let new_service: CarritoService = {
+                    precargado: true,
+                    servicio_rowid: service.id,
+                    servicio_id: service.id,
+                    servicio_codigo: service.model_servicio_id.codigo,
+                    numero_producto: service.numero_producto,
+                    servicio_precio_final_clp: service.valor_unitario_clp,
+                    servicio_nombre: service.model_servicio_id.descripcion,
+                    tipo_servicio_descripcion:
+                      service.model_tipo_servicio_id.descripcion,
+                    tipo_servicio_id: service.tipo_servicio_id,
+                    servicio_cantidad: service.cantidad,
+                    servicio_unidad_cod: service.model_unidad_id.codigo,
+                    servicio_unidad_descripcion:
+                      service.model_unidad_id.descripcion,
+                    prov_has_serv_precio: service.prov_has_serv_precio,
+                    puntos_baremos: service.puntos_baremos,
+                    unidad_obras: [
+                      {
+                        precargado: true,
+                        uo_rowid: uo.id,
+                        uo_codigo: uo.unidad_obra_cod,
+                        uo_nombre: uo.model_unidad_obra_cod.descripcion,
+                        uo_precio_total_clp: uo.valor_unitario_clp,
+                        actividad_descripcion:
+                          service.model_actividad_id.descripcion,
+                        actividad_id: service.model_actividad_id.id,
+                        uo_cantidad: uo.cantidad,
+                        uob_unidad_medida_cod: uo.model_unidad_id.codigo,
+                        uob_unidad_medida_descripcion:
+                          uo.model_unidad_id.descripcion,
+                      },
+                    ],
+                  };
+
+                  this.dataCubicacionIngenieria.push(new_service);
+                });
+              }
+            );
+          }
+        }
+      )
+    );
+  }
+
+  serviciosAdicionalesAprobados(): boolean {
+    if (this.tableServiciosAutorizarAdicionales?.formTable) {
+      let form = this.tableServiciosAutorizarAdicionales.formTable.get('table')
+        .value as Array<{
+        validar_adicional: boolean;
+      }>;
+      if (form.length > 0) {
+        let serviciosRechazados = form.filter(
+          servicio => servicio.validar_adicional
+        );
+
+        return serviciosRechazados.length === 0;
+      } else {
+        return true;
+      }
+    }
+    return true;
+  }
+
+  accionExist(accion: string): boolean {
+    return this.accionesOT.find(v => v.slug === accion) !== undefined;
   }
 
   ngOnDestroy(): void {
