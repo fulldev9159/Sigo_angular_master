24d8f009948968754565e466b677b17cbccd6881
Jorge R <jorge.retamal@zweicom.com>
2022-12-01 09:46:08 -0300

    agregar servicios ya agregados al carrito reporte ingenieria #323

--- src/app/features/ingenieria/containers/resultado-ingenieria-container/resultado-ingenieria-container.component.ts
+++ src/app/features/ingenieria/containers/resultado-ingenieria-container/resultado-ingenieria-container.component.ts
@@ -152,8 +152,52 @@ export class ResultadoIngenieriaContainerComponent
             this.cubicacionFacade.proveedorSelected(proveedor);
           }
 
-          if (cubicacionIngenieria) {
-            // console.log(cubicacionIngenieria);
+          if (
+            cubicacionIngenieria &&
+            cubicacionIngenieria.data.many_cubicacion_has_servicio
+          ) {
+            // console.log(cubicacionIngenieria)
+            cubicacionIngenieria.data.many_cubicacion_has_servicio.forEach(
+              (service: DetalleServicioCubicacion) => {
+                service.many_cubicacion_has_uob.forEach(uo => {
+                  let new_service: CarritoService = {
+                    precargado: true,
+                    servicio_rowid: service.id,
+                    servicio_id: service.id,
+                    servicio_codigo: service.model_servicio_id.codigo,
+                    numero_producto: service.numero_producto,
+                    servicio_precio_final_clp: service.valor_unitario_clp,
+                    servicio_nombre: service.model_servicio_id.descripcion,
+                    tipo_servicio_descripcion:
+                      service.model_tipo_servicio_id.descripcion,
+                    tipo_servicio_id: service.tipo_servicio_id,
+                    servicio_cantidad: service.cantidad,
+                    servicio_unidad_cod: service.model_unidad_id.codigo,
+                    servicio_unidad_descripcion:
+                      service.model_unidad_id.descripcion,
+                    prov_has_serv_precio: service.prov_has_serv_precio,
+                    puntos_baremos: service.puntos_baremos,
+                    unidad_obras: [
+                      {
+                        precargado: true,
+                        uo_rowid: uo.id,
+                        uo_codigo: uo.unidad_obra_cod,
+                        uo_nombre: uo.model_unidad_obra_cod.descripcion,
+                        uo_precio_total_clp: uo.valor_unitario_clp,
+                        actividad_descripcion:
+                          service.model_actividad_id.descripcion,
+                        actividad_id: service.model_actividad_id.id,
+                        uo_cantidad: uo.cantidad,
+                        uob_unidad_medida_cod: uo.model_unidad_id.codigo,
+                        uob_unidad_medida_descripcion:
+                          uo.model_unidad_id.descripcion,
+                      },
+                    ],
+                  };
+                  this.serviciosFacade.addDirectServiceCarrito(new_service);
+                });
+              }
+            );
           }
         }
       )
@@ -180,6 +224,7 @@ export class ResultadoIngenieriaContainerComponent
 
     this.subscription.add(
       this.carrito$.pipe(take(1)).subscribe(carrito => {
+        console.log(carrito);
         const isLocal = (item: { precargado?: boolean }) =>
           item.precargado === undefined || item.precargado === false;
 
