057f8b56f74108dc9e97a9de7c993219c89f0ca6
Jorge R <jorge.retamal@zweicom.com>
2022-12-10 16:31:54 -0300

    selenium

--- script/index.py
+++ script/index.py
@@ -5,6 +5,10 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from time import sleep
+
 
 data = ''
 driver = ''
@@ -40,13 +44,14 @@ def openBrowser():
     global driver
     # Create a new instance of the Chrome web browser
     driver = webdriver.Chrome()
+    driver.maximize_window()
 
     # Open the web page
     driver.get("http://{0}:{1}".format(data["environment"]["host"],
                data["environment"]["port"]))
 
     driver.set_page_load_timeout(30)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(2)
 
 
 def crear_cubicacion_nueva():
@@ -72,6 +77,21 @@ def crear_cubicacion_nueva():
         '#select-proveedor',
         cub['proveedor']
     )
+
+    servicios = cub['servicios']
+    for item in servicios:
+        _select_dropdown('#select-actividad', item['actividad'])
+        _select_dropdown('#select-tipo-servicio', item['tipo_servicio'])
+        _searh_item_and_select('#select-servicio', item['nombre'])
+        uos = item['uos']
+        for uo in uos:
+            _searh_uo_and_add('#select-unidad-obra', uo['nombre'])
+            _change_cantidad(item['nombre'], item['cantidad'])
+            _change_cantidad(uo['nombre'], uo['cantidad'])
+
+    element = driver.find_element(
+        By.CSS_SELECTOR, 'button[id="crear-cubicacion"]')
+    element.click()
 
 
 def login(username, perfil):
@@ -110,12 +130,88 @@ def _select_dropdown(selector, selection):
 def _input(selector, text):
     search_field = driver.find_element(
         By.CSS_SELECTOR, selector)
+    search_field.clear()
     search_field.send_keys(text)
 
 
 def get_element_attr(selector):
     element = driver.find_element(By.CSS_SELECTOR, selector)
     return element.get_attribute("ng-reflect-options")
+
+
+def _searh_item_and_select(selector, item):
+    wait = WebDriverWait(driver, 10)
+    # Wait for the callable function to return a non-empty value
+    wait.until(lambda driver: get_element_attr(selector))
+
+    try:
+        dropdown = driver.find_element(
+            By.CSS_SELECTOR, '{0}> div > i'.format(selector))
+        dropdown.click()
+
+        dropdown = driver.find_element(By.CSS_SELECTOR, selector)
+        dropdown.click()
+    except NoSuchElementException:
+        dropdown = driver.find_element(By.CSS_SELECTOR, selector)
+        dropdown.click()
+        pass
+
+    _input('{0}>div>div:nth-child(4)>div:nth-child(1)>div>input'.format(selector), item)
+    elements = driver.find_elements(By.CSS_SELECTOR, 'p-dropdownitem > li')
+    print(f'c=', len(elements))
+    if len(elements) == 1:
+        elements[0].click()
+    else:
+        parser.error("Se encontraron {0} items para {1}".format(
+            len(elements), item))
+
+
+def _searh_uo_and_add(selector, item):
+    wait = WebDriverWait(driver, 10)
+    # Wait for the callable function to return a non-empty value
+    wait.until(lambda driver: get_element_attr(selector))
+
+    try:
+        dropdown = driver.find_element(
+            By.CSS_SELECTOR, '{0}> div > i'.format(selector))
+        dropdown.click()
+
+        dropdown = driver.find_element(By.CSS_SELECTOR, selector)
+        dropdown.click()
+    except NoSuchElementException:
+        dropdown = driver.find_element(By.CSS_SELECTOR, selector)
+        dropdown.click()
+        pass
+
+    _input('{0}>div>div:nth-child(4)>div:nth-child(1)>div>input'.format(selector), item)
+    elements = driver.find_elements(By.CSS_SELECTOR, 'p-dropdownitem > li')
+    if len(elements) == 1:
+        elements[0].click()
+        search_field_button = driver.find_element(
+            By.CSS_SELECTOR, "#agregar-button")
+        try:
+            sleep(1)
+            search_field_button.click()
+        except StaleElementReferenceException:
+            # Re-find the element and click it again
+            search_field_button = driver.find_element(
+                By.CSS_SELECTOR, "#agregar-button")
+            sleep(1)
+            search_field_button.click()
+    else:
+        parser.error("Se encontraron {0} items para {1}".format(
+            len(elements), item))
+
+
+def _change_cantidad(item, cantidad):
+    if cantidad != 0 and cantidad != '0':
+        # Find the siblings of the element using XPath
+        xpath = f"//td[contains(text(), '{{0}}')]/following-sibling::*".format(
+            item)
+        elements = driver.find_elements(By.XPATH, xpath)
+        inp = elements[2].find_element(By.CSS_SELECTOR, 'input')
+        inp.clear()
+        inp.send_keys(cantidad)
 
 
 def main():
